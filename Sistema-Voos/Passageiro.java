cd03b1d9951e00d011be99e6bc2e858172fca0c3
Tales Buzanello <69158760+TalesBuzanello@users.noreply.github.com>
2025-09-12 21:27:01 -0300

    Create Passageiro.java

---
+++ Sistema-Voos/Passageiro.java
@@ -0,0 +1,68 @@
+public class Passageiro {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String email;
+    private String telefone;
+    private String nacionalidade;
+    private String genero; 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getData_nascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+        public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+
+}
